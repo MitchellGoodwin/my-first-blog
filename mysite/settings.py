d572382df89a6c12342908ab5a7c89220222cf91
github username <github email address>
2020-01-29 10:54:55 -0500

    Changed the settings.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!b$$+ni-y2di+atz6v4s)v5wj%)l5###9^!mowi)&qwgs2&a#r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
